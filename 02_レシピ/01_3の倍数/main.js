3bc9607c1e153d2fdb70fbfa42f3063a3785d599
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-03 23:31:58 +0900

    3の倍数_エラー修正

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,7 +1,5 @@
 const maxNum = 24
 
-genkiFunc(maxNum)
-
 const genkiFunc = (maxNum) => {
   for (let i = 1; i <= maxNum; i++) {
     console.log(specialStrOnly3Multiple(i))
@@ -11,3 +9,5 @@ const genkiFunc = (maxNum) => {
 const specialStrOnly3Multiple = (num) => {
   return num % 3 === 0 ? `${num}!!!!!` : num
 }
+
+genkiFunc(maxNum)
