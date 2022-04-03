757855ba970fb7526c6ce1f99adb9cbeb4b4d471
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-04 00:06:03 +0900

    3が大好き

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,9 @@
-const FizzBuzz = (num) => {
-  return num % 15 == 0
-    ? "FizzBuzz"
-    : num % 5 == 0
-    ? "Buzz"
-    : num % 3 == 0
-    ? "Fizz"
-    : num
+const specialStrWhen3 = (num) => {
+  /**3の倍数もしくは3を含む時に特殊な文字列を返します */
+  return !(num % 3) || String(num).includes("3") ? `${num}!!!!!` : num
 }
 
-const maxNum = 30
-for (let i = 1; i <= maxNum; i++) console.log(FizzBuzz(i))
+const maxNum = 40
+for (let i = 1; i <= maxNum; i++) {
+  console.log(specialStrWhen3(i))
+}
