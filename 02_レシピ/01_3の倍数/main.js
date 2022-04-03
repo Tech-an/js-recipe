7e6a28d98a134713548576be7fbfc83d31a991df
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-04 01:05:19 +0900

    元気な秒針

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -3,7 +3,13 @@ const specialStrWhen3 = (num) => {
   return !(num % 3) || String(num).includes("3") ? `${num}!!!!!` : num
 }
 
-const maxNum = 40
-for (let i = 1; i <= maxNum; i++) {
-  console.log(specialStrWhen3(i))
+function genkiFunc(endNum = -1, delay = 1000) {
+  let [num, loopNum] = [1, endNum]
+  const intervalId = setInterval(() => {
+    console.log(specialStrWhen3(num++))
+    if (--loopNum == 0) clearInterval(intervalId)
+  }, delay)
 }
+
+genkiFunc(13)
+// genkiFunc()
