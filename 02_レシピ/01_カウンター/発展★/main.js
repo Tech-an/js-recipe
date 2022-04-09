8914085c9b012837d785c09a10c220df148d9542
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-09 12:58:14 +0900

    カウンター発展★の提出

---
+++ 02_レシピ/01_カウンター/発展★/main.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const multiplyButton = document.getElementById("multiply-button")
+
+let count = 0
+
+plusButton.onclick = () => {
+  count += 1
+  display.textContent = count
+}
+
+minusButton.onclick = () => {
+  count -= 1
+  display.textContent = count
+}
+
+multiplyButton.onclick = () => {
+  count *= 2
+  display.textContent = count
+}
