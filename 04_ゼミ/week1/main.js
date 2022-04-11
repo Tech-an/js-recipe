e39f434f725bcacfd0eb7163c21460e4aa5d5edc
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-12 08:47:36 +0900

    第1回もくもく会のメモ

---
+++ 04_ゼミ/week1/main.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("nice-button")
+button.onclick = function () {
+  console.log("こんにちは")
+}
+
+// let height = prompt("height?")
+// let weight = prompt("weight?")
+
+// &&のパターン
+// if (weight > 70 && height < 170) {
+//   alert("肥満です")
+// }
+
+// ||のパターン
+// if (weight > 70 || height < 170) {
+//     alert("肥満かもね")
+// }
