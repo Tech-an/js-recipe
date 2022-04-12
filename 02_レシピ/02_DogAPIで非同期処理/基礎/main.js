c5f881134656d13a30a6f5987d5a03e81053feec
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-12 15:14:21 +0900

    DogAPIのフォルダ構造を変更

--- 02_レシピ/02_DogAPIで非同期処理/基礎/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/基礎/main.js
@@ -5,6 +5,7 @@ fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => {
     return res.json() // 結果を json として読み込んで、次の then に渡す
   })
+  //1つめのthenでreturnした値が2つめのthenの引数(data)に渡されている
   .then((data) => {
     imageElement.src = data.message // 画像を表示する
   })
