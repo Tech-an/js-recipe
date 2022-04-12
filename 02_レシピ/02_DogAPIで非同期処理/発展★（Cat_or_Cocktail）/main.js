df086f8fd063b04ae5b4f0c1ef626a1a04ba5565
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-04-12 15:12:56 +0900

    発展★（CatAndCocktail）

---
+++ 02_レシピ/02_DogAPIで非同期処理/発展★（Cat_or_Cocktail）/main.js
@@ -0,0 +1,19 @@
+const catImageElement = document.getElementById("cat-image")
+const cocktailImageElement = document.getElementById("cocktail-image")
+
+// 指定したサーバーにデータを取りに行く
+fetch("https://api.thecatapi.com/v1/images/search")
+  .then((res) => {
+    return res.json() // 結果を json として読み込んで、次の then に渡す
+  })
+  .then((data) => {
+    catImageElement.src = data[0].url // 画像を表示する
+  })
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    cocktailImageElement.src = data.drinks[0].strDrinkThumb
+  })
