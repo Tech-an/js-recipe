ea3a8d8b0754c87819a1abb7d15831db93675b3d
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2022-05-20 15:27:23 +0900

    復習のためもう1回初期値カウンターやってみた

---
+++ 06_Vue/re_fashionable_human/src/components/CounterButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- ボタンを作る -->
+  <button v-on:click="countUp"><slot></slot></button>
+  <div>
+    <p>カウント</p>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  props: {
+    defaultNum: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      count: this.defaultNum,
+    }
+  },
+  methods: {
+    countUp: function () {
+      this.count += 1
+    },
+  },
+}
+</script>
